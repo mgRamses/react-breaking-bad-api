a29001eb912442bd2bd438b48da67d5991c7513d
Rmasés Mendoza <mg.ramses@gmail.com>
2020-07-04 23:01:25 -0500

    Go ahead

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
@@ -53,7 +28,6 @@ const App = () => {
     <Search getQuery={(q) => setQuery(q)}/>
     <CharacterGrid isLoading={isLoading} items={items}/>
   </div>
->>>>>>> Here we go
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-=======
-import App from './App';
->>>>>>> Here we go
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-<<<<<<< HEAD
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
-);
->>>>>>> Here we go
